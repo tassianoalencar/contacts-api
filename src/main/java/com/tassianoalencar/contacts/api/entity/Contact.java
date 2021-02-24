0a17c9930c3da93d14dbfe8dfc69065f2ee910d9
Tassiano Alencar <tassiano.alencar@icloud.com>
2021-02-24 09:10:10 -0300

    Create fake datas and fix table name

--- src/main/java/com/tassianoalencar/contacts/api/entity/Contact.java
+++ src/main/java/com/tassianoalencar/contacts/api/entity/Contact.java
@@ -1,12 +1,10 @@
 package com.tassianoalencar.contacts.api.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Table(name = "contacts")
 public class Contact {
 
     @Id
@@ -14,6 +12,9 @@ public class Contact {
     private Long id;
     private String name;
     private String phoneNumber;
+
+    public Contact() {
+    }
 
     public Contact(String name, String phoneNumber) {
         this.name = name;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO contacts (name, phone_number) VALUES ('Person 01', '00000000000');
+INSERT INTO contacts (name, phone_number) VALUES ('Person 02', '11111111111');
