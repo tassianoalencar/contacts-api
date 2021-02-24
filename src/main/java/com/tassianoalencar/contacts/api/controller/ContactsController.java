02256ed81662ada4f774ace9aed80b62e503ae3e
Tassiano Alencar <tassiano.alencar@icloud.com>
2021-02-24 09:02:11 -0300

    Create endpoint to return contacts paginated

--- src/main/java/com/tassianoalencar/contacts/api/controller/ContactsController.java
+++ src/main/java/com/tassianoalencar/contacts/api/controller/ContactsController.java
@@ -1,16 +1,35 @@
 package com.tassianoalencar.contacts.api.controller;
 
+import com.tassianoalencar.contacts.api.entity.Contact;
+import com.tassianoalencar.contacts.api.repository.ContactRepository;
+import com.tassianoalencar.contacts.api.response.ContactResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/contacts")
 public class ContactsController {
 
-    @GetMapping
-    public String getAllContacts() {
-        return "Hello Contacts";
-    }
+    @Autowired
+    private ContactRepository contactRepository;
 
+    @GetMapping
+    public Page<ContactResponse> getContactsPaginated(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "5") int size) {
+
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<Contact> pageResult = contactRepository.findAll(pageRequest);
+        List<ContactResponse> contacts = ContactResponse.convert(pageResult);
+
+        return new PageImpl<>(contacts, pageRequest, pageResult.getTotalElements());
+    }
 }
