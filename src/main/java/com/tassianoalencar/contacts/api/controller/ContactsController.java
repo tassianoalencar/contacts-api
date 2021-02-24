ba7c46fdb85110d9e5ed61dbaac7d70361e8da94
Tassiano Alencar <tassiano.alencar@icloud.com>
2021-02-24 10:26:53 -0300

    Create post endpoint

--- src/main/java/com/tassianoalencar/contacts/api/controller/ContactsController.java
+++ src/main/java/com/tassianoalencar/contacts/api/controller/ContactsController.java
@@ -2,16 +2,19 @@ package com.tassianoalencar.contacts.api.controller;
 
 import com.tassianoalencar.contacts.api.entity.Contact;
 import com.tassianoalencar.contacts.api.repository.ContactRepository;
+import com.tassianoalencar.contacts.api.request.ContactRequest;
 import com.tassianoalencar.contacts.api.response.ContactResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -31,5 +34,15 @@ public class ContactsController {
         List<ContactResponse> contacts = ContactResponse.convert(pageResult);
 
         return new PageImpl<>(contacts, pageRequest, pageResult.getTotalElements());
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<ContactResponse> createContact(@RequestBody @Valid ContactRequest contactRequest, UriComponentsBuilder uriComponentsBuilder) {
+        Contact contact = ContactRequest.convert(contactRequest);
+        contactRepository.save(contact);
+
+        URI uri = uriComponentsBuilder.path("/api/v1/contacts/{id}").buildAndExpand(contact.getId()).toUri();
+        return ResponseEntity.created(uri).body(new ContactResponse(contact));
     }
 }
--- src/test/java/com/tassianoalencar/contacts/api/controller/ContactsControllerTest.java
+++ src/test/java/com/tassianoalencar/contacts/api/controller/ContactsControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,5 +27,18 @@ class ContactsControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("phoneNumber")));
+    }
+
+    @Test
+    public void onlyContactValidShouldBeCreated() throws Exception {
+        String mockJsonContactInvalid = "{\"name\": \"Person Name\", \"phoneNumber\": \"\"}";
+
+        mockMvc.perform(
+                post("/api/v1/contacts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mockJsonContactInvalid))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+
     }
 }
